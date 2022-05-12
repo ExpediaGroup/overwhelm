8c95238f1d2fd2f7c911e96fa8de7379a10e6875
Sangeetha Madamanchi <sdamodar@homeaway.com>
2022-05-12 10:48:29 -0500

    RCP-255: removed a reference that was added incorrectly

--- main.go
+++ main.go
@@ -82,13 +82,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Application")
 		os.Exit(1)
 	}
-	if err = (&controllers.SourceReferenceReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "SourceReference")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
