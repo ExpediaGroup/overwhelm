114b4d37885f5f967afb354a3dfe62dd11e318d3
TwiN <chris@twin.sh>
2022-07-05 18:06:51 -0400

    feat(analyzer): Check both init and non-init containers

--- analyzer/pod.go
+++ analyzer/pod.go
@@ -35,7 +35,7 @@ func Pod(pod v1.Pod) Result {
 		ResourceName: pod.Name,
 		Healthy:      true,
 	}
-	// Check the conditions (unless it's a job or a finite
+	// Check the conditions
 	if pod.Status.Phase != v1.PodSucceeded {
 		for _, condition := range pod.Status.Conditions {
 			if condition.Type == v1.PodReady && condition.Status != v1.ConditionTrue {
@@ -44,24 +44,37 @@ func Pod(pod v1.Pod) Result {
 			}
 		}
 	}
-	// Check the container states
-	for _, container := range pod.Status.ContainerStatuses {
-		if container.State.Running != nil && (container.State.Terminated != nil && container.State.Terminated.Reason != "Completed") {
-			result.Healthy = false
-		}
-		if container.State.Waiting != nil {
-			result.Errors = append(result.Errors, formatContainerError(container.Name, "waiting", container.State.Waiting.Reason, container.State.Waiting.Message, 0))
-		} else if container.State.Terminated != nil && container.State.Terminated.Reason != "Completed" {
-			result.Errors = append(result.Errors, formatContainerError(container.Name, "terminated", container.State.Terminated.Reason, container.State.Terminated.Message, container.State.Terminated.ExitCode))
-		}
-	}
+	// Check init container statuses.
+	analyzeContainers(pod.Status.InitContainerStatuses, &result)
+	// Check container statuses. Note that if any init containers are not ready, the non-init containers will be stuck in a waiting state.
+	analyzeContainers(pod.Status.ContainerStatuses, &result)
 	return result
 }
 
-func formatContainerError(containerName, state, reason, message string, exitCode int32) string {
+func analyzeContainers(containerStatuses []v1.ContainerStatus, result *Result) {
+	for _, container := range containerStatuses {
+		if !container.Ready {
+			result.Healthy = false
+		}
+		if container.State.Waiting != nil {
+			result.Errors = append(result.Errors, formatContainerError(container.Name, "waiting", container.State.Waiting.Reason, container.State.Waiting.Message, 0, container.Ready))
+		} else if container.State.Terminated != nil && container.State.Terminated.Reason != "Completed" {
+			result.Errors = append(result.Errors, formatContainerError(container.Name, "terminated", container.State.Terminated.Reason, container.State.Terminated.Message, container.State.Terminated.ExitCode, container.Ready))
+		} else if container.State.Running != nil && !container.Ready {
+			result.Errors = append(result.Errors, formatContainerError(container.Name, "running", "", "", 0, container.Ready))
+		}
+	}
+}
+
+func formatContainerError(containerName, state, reason, message string, exitCode int32, ready bool) string {
 	sb := new(strings.Builder)
 	if len(containerName) > 0 {
 		sb.WriteString(fmt.Sprintf("container '%s'", containerName))
+	}
+	if !ready {
+		sb.WriteString(" is not ready and")
+	} else {
+		sb.WriteString(" is ready and")
 	}
 	if len(state) > 0 {
 		sb.WriteString(fmt.Sprintf(" is in a %s state", state))
