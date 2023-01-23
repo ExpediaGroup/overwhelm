fc0356efcf5aa1717324fb4f9d5f95a0415adfd8
Sangeetha Madamanchi <smadamanchi@expediagroup.com>
2023-01-23 15:20:02 -0600

    fix: added rbac to create and update events, automated chart upgrade and image pull secrets

--- scripts/helm-charts/update.go
+++ scripts/helm-charts/update.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -10,12 +13,48 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	data := string(d)
-	data = strings.ReplaceAll(data, "{{", "\\{\\{")
-	data = strings.ReplaceAll(data, "<GOTEMPLATE>", "{{")
-	data = strings.ReplaceAll(data, "</GOTEMPLATE>", "}}")
+	//if condition string patch. The kustomize created a sub element to spec element since we cant add this block to the spec key directly. This bit
+	//will remove the sub element and indent this block back to the spec element
+	ifString := regexp.MustCompile("{{- if(.|\n)+end }}").FindString(data)
+	ifIndentPatch := regexp.MustCompile("\n\\s\\s").ReplaceAllString(ifString, "\n")
+	data = regexp.MustCompile("\"\": \\|-\\s+").ReplaceAllString(data, "")
+	data = regexp.MustCompile("{{- if(.|\n)+end }}").ReplaceAllString(data, ifIndentPatch)
+
+	data = strings.ReplaceAll(data, "'{{", "{{")
+	data = strings.ReplaceAll(data, "}}'", "}}")
 	data = strings.ReplaceAll(data, "{{ toYaml", "\n{{ toYaml")
+	data = regexp.MustCompile("{{\\s+").ReplaceAllString(data, "{{ ")
+	data = regexp.MustCompile("\\s+}}").ReplaceAllString(data, " }}")
+
 	if err = os.WriteFile("charts/tmp.yaml", []byte(data), 0644); err != nil {
 		panic(err)
+	}
+
+	c, err := os.ReadFile("charts/overwhelm/Chart.yaml")
+	if err != nil {
+		panic(err)
+	}
+	chart := string(c)
+	chartVersion := regexp.MustCompile("(version: )(.+)(\n)").FindSubmatch(c)[2]
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("The current chart version is %s. Provide the new version in the form of x.x.x if applicable or type NA if not upgrade is needed \n", chartVersion)
+	scanner.Scan()
+	text := scanner.Text()
+	if strings.ToLower(text) != "na" {
+		fmt.Printf("The new version will be version: %s. Type Yes to confirm: ", text)
+		scanner.Scan()
+		confirm := scanner.Text()
+		if strings.ToLower(confirm) != "yes" {
+			fmt.Printf("Ok, abandoning chart upgrade \n")
+		} else {
+			newVersion := "version: " + text
+			chart = strings.ReplaceAll(chart, "version: "+string(chartVersion), newVersion)
+			if err = os.WriteFile("charts/overwhelm/Chart.yaml", []byte(chart), 0644); err != nil {
+				panic(err)
+			}
+			fmt.Printf("Chart version upgraded to %s \n", text)
+		}
 	}
 }
